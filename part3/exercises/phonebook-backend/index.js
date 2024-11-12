abea9e62c87358d3735603b31a777afbbf61efa9
Pham Hai Minh <phminh2105@gmail.com>
2024-11-12 23:20:53 +0800

    Added exercises 3.1 - 3.5 for part 3

---
+++ part3/exercises/phonebook-backend/index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.post('/api/persons', (req, res) => {
+    const id = String(Math.floor(Math.random() * 100) + 10);
+    const body = req.body;
+
+    if(!body.name){
+        return res.status(400).json({
+            error: "name is missing"
+        });
+    } else if (persons.some(person => person.name === body.name)){
+        return res.status(400).json({
+            error: "name must be unique"
+        });
+    } else if(!body.number){
+        return res.status(400).json({
+            error: "number is missing"
+        });
+    } else {
+        const newPerson = {
+            id,
+            'name': body.name,
+            'number': body.number
+        }
+
+        persons = persons.concat(newPerson)
+
+        res.json(newPerson)
+    }
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(person => person.id === id)
+    if (person){
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    persons = persons.filter(person => person.id !== id)
+    res.json(persons)
+})
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001')
+})
+
+    
