5256ca07cf87c6908e2ea74a8387a0ab4e3d9107
Pham Hai Minh <phminh2105@gmail.com>
2024-11-13 15:13:31 +0800

    Deploying app to the internet

--- part3/part3-study-material/index.js
+++ part3/part3-study-material/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 let notes = [
   {
@@ -73,7 +77,13 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
